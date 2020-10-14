d5e510041e0a279026a5a401828c374d01e1b542
Fabrício Gustavo Henrique <fabricio.henrique@fatec.sp.gov.br>
2020-10-13 22:23:19 -0300

    Subindo Projeto

---
+++ WebApplication2/Models/Endereco.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class Endereco
+    {
+        public string Descricao { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string UF { get; set; }
+
+    }
+}
